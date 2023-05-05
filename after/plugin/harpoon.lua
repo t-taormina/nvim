ffcc317c44045b3a4cdf785576054509c3fb5b85
tyler <tktaormina@gmail.com>
2023-05-05 14:35:25 -0700

    Adding a harpoon change

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,3 +8,4 @@ vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-e>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-i>", function() ui.nav_file(4) end)
+
