29c525c2ec3af0c8abd7f041ea90fc43783e15cd
t-taormina <tktaormina@gmail.com>
2023-05-05 08:46:02 -0700

    added my plugin and compiled.

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/tylr/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/tylr/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/tylr/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/tylr/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/tylr/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/tylrr/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/tylrr/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/tylrr/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/tylrr/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/tylrr/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,92 +76,92 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["colemak-neovim"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/colemak-neovim",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/colemak-neovim",
     url = "https://github.com/t-taormina/colemak-neovim"
   },
   harpoon = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/harpoon",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/theprimeagen/harpoon"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   playground = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/playground",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/playground",
     url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["rose-pine"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/rose-pine",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/rose-pine",
     url = "https://github.com/rose-pine/neovim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   undotree = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/undotree",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
   ["vim-floaterm"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/vim-floaterm",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/vim-floaterm",
     url = "https://github.com/voldikss/vim-floaterm"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/home/tylr/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/Users/tylrr/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   }
 }
