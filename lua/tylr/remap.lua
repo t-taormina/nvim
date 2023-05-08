77fe65eac6c20e76e27d5a9937675ce749a26806
tyler <tktaormina@gmail.com>
2023-05-08 09:57:16 -0700

    updating some keybinds for telescope

--- lua/tylr/remap.lua
+++ lua/tylr/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 
+vim.keymap.set("n", "/", "/\v")
+vim.keymap.set("v", "/", "/\v")
+
 vim.keymap.set("v", "N", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "E", ":m '<-2<CR>gv=gv")
 
